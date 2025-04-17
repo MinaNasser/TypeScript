9fb0b4bbc13668bb56432b30490c58b732accc55
MinaAl5al <minanasser82018@gmail.com>
2025-04-17 09:44:33 +0200

    constructor

--- Day1/demo/Person.ts
+++ Day1/demo/Person.ts
@@ -3,11 +3,17 @@ class Person {
     public Phone: string;
     private Age: number;
     protected IsMale: boolean;
-    Display(): void {
+    publicDisplay(): void {
         console.log(this.Name);
         console.log(this.Phone);
         console.log(this.Age);
         console.log(this.IsMale);
     }
-    
+    public constructor(name: string, phone: string, age: number, isMale: boolean) {
+        this.Name = name;
+        this.Phone = phone;
+        this.Age = age;
+        this.IsMale = isMale;
+    }
+
 }
