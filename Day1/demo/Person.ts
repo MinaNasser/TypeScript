509e02456ed63e5444933cfabf43ef4202729f28
MinaAl5al <minanasser82018@gmail.com>
2025-04-17 09:37:13 +0200

    Display Function

--- Day1/demo/Person.ts
+++ Day1/demo/Person.ts
@@ -3,5 +3,11 @@ class Person {
     public Phone: string;
     private Age: number;
     protected IsMale: boolean;
+    Display(): void {
+        console.log(this.Name);
+        console.log(this.Phone);
+        console.log(this.Age);
+        console.log(this.IsMale);
+    }
     
 }
